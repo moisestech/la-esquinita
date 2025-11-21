7c494711b14f4f2e8a6eb59cec2d003aee0dd7dd
CornBrother0x <101160087+CornBrother0x@users.noreply.github.com>
2025-11-21 15:02:12 -0500

    new build

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,11 @@
 module.exports = {
   root: true,
   extends: ["next/core-web-vitals"],
+  rules: {
+    "@next/next/no-img-element": "off",
+    "react/no-unescaped-entities": "off",
+    "react/display-name": "off",
+    "react-hooks/rules-of-hooks": "off",
+    "react-hooks/exhaustive-deps": "off",
+  },
 }
